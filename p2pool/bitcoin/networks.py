576940e8792b6653b0bf8a29aa8cb02832c72cf1
CartmanSPC <CartmanSPC@server.fake>
2014-02-22 03:06:55 -0800

    nyancoin
    NON-STOP NYAN COIN!

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -320,6 +320,28 @@ nets = dict(
         DUST_THRESHOLD=1e8,
     ),
 
+    nyancoin=math.Object(
+        P2P_PREFIX='fcd9b7dd'.decode('hex'),
+        P2P_PORT=33701,
+        ADDRESS_VERSION=45,
+        RPC_PORT=33700,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'nyancoin address' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 337*100000000 >> (height + 1)//500000,
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
+        BLOCK_PERIOD=60, # s targetspacing
+        SYMBOL='NYAN',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'nyancoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/nyancoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.nyancoin'), 'nyancoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://nyancha.in/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://nyancha.in/address/',
+        TX_EXPLORER_URL_PREFIX='http://nyancha.in/tx/',
+        SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
+        DUMB_SCRYPT_DIFF=2**16,
+        DUST_THRESHOLD=1e8,
+    ),
+
     potcoin=math.Object(
         P2P_PREFIX='fbc0b6db'.decode('hex'),
         P2P_PORT=4200,
--- p2pool/networks.py
+++ p2pool/networks.py
@@ -237,6 +237,25 @@ nets = dict(
         VERSION_CHECK=lambda v: True,
     ),
 
+    nyancoin=math.Object(
+        PARENT=networks.nets['nyancoin'],
+        SHARE_PERIOD=10, # seconds target spacing
+        CHAIN_LENGTH=24*60*60//10, # shares
+        REAL_CHAIN_LENGTH=24*60*60//10, # shares
+        TARGET_LOOKBEHIND=200, # shares coinbase maturity
+        SPREAD=30, # blocks
+        IDENTIFIER='5D4D5622297EE0E4'.decode('hex'),
+        PREFIX='4DD804B010424A99'.decode('hex'),
+        P2P_PORT=23670,
+        MIN_TARGET=0,
+        MAX_TARGET=2**256//2**20 - 1,
+        PERSIST=False,
+        WORKER_PORT=8870,
+        BOOTSTRAP_ADDRS='xpool.net'.split(' '),
+        ANNOUNCE_CHANNEL='#p2pool-nyan',
+        VERSION_CHECK=lambda v: True,
+    ),
+
     potcoin=math.Object(
         PARENT=networks.nets['potcoin'],
         SHARE_PERIOD=10, # seconds target spacing
