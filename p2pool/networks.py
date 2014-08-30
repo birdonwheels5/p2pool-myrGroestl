74ae484394d5663e18c83453852c62b5afbce4d0
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-08-30 17:03:35 -0400

    Update networks.py

--- p2pool/networks.py
+++ p2pool/networks.py
@@ -269,7 +269,7 @@ nets = dict(
         MAX_TARGET=2**256//2**20 - 1,
         PERSIST=True,
         WORKER_PORT=3333,
-        BOOTSTRAP_ADDRS='birdspool.no-ip.org aforis.mooo.com us.myriadminers.com eu.myriadminers.com'.split(' '),
+        BOOTSTRAP_ADDRS='birdspool.no-ip.org aforis.mooo.com us.myriadminers.com eu.myriadminers.com nz.p2pool.geek.nz'.split(' '),
         #ANNOUNCE_CHANNEL='#cryptopools-p2p',
         VERSION_CHECK=lambda v: True,
     ),
